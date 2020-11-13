6d937a69e4954e22b7cc3be5ecf6b832d2f06f66
Blealtan Cao <blealtan@outlook.com>
2020-11-13 18:21:53 +0800

    Reformat code, filter out non-live videos, automatic crawler loop

--- client.py
+++ client.py
@@ -1,51 +1,74 @@
 from crawler import *
 from threading import Thread
 import json
- 
-month_range = [10, 10]
+import jsonstreams
+import requests
+import multiprocessing
 
-def pull_video(video_id):    
+month_range = [5, 10]
+
+
+def pull_video(video_id):
     try:
-        
+
         v_data = pull_video_data(video_id)
         c_data = pull_chat_data(video_id)
-        v_data["chat_analyzed"]= c_data["chat_analyzed"]
+        v_data["chat_analyzed"] = c_data["chat_analyzed"]
 
-        f = open("data/video_data.json", "wb")
-        f.write(json.dumps(v_data, ensure_ascii=False, indent=4).encode('UTF-8'))
+        # f = open("data/video_data.json", "wb")
+        # f.write(json.dumps(v_data, ensure_ascii=False, indent=4).encode('UTF-8'))
     except requests.RequestException as e:
         print(e)
     return v_data
 
-def pull_streamer_data(name, streamer_id, month_range):#爬取ID为streamer_id的直播主month_range内的视频数据
+
+# 爬取ID为streamer_id的直播主month_range内的视频数据
+def pull_streamer_data(name, streamer_id, month_range):
     print("pulling " + name)
-    
-    main_list = []#主数据表
-    f = open("data/" + name + ".json", "wb")
-    
-    video_id_list = pull_video_id(streamer_id, month_range)#爬取ID为streamer_id的直播主month_range内的视频数据
-    for v in video_id_list:
-        v_data = pull_video(v)
-        main_list.append(v_data)
-     
-    f.write(json.dumps(main_list, ensure_ascii=False, indent=4).encode('UTF-8'))
+    with jsonstreams.Stream(jsonstreams.Type.array, "crawled/" + name + ".json", indent=2, pretty=True) as s:
+        # 爬取ID为streamer_id的直播主month_range内的视频数据
+        video_id_list = pull_video_id(streamer_id, month_range)
+        for v in video_id_list:
+            v_data = pull_video(v)
+            s.iterwrite(v_data.items())
     print(name + " saved")
 
+
 def pull_streamer(streamer_id):
-        streamer_list= json.load(open("data/VTuber.json"))['result']
-        for streamer in streamer_list:
-            if streamer_id == streamer["streamer_id"]:
-                pull_streamer_data(streamer["name"], streamer_id, month_range)
+    streamer_list = json.load(open("data/VTuber.json"))['result']
+    for streamer in streamer_list:
+        if streamer_id == streamer["streamer_id"]:
+            pull_streamer_data(streamer["name"], streamer_id, month_range)
 
-# g = requests.post(group_url, headers=headers, data=json.dumps({'filter_state': '{"text": "", "inc_old_group": false, "retired" : "all", "following":false, "notifications": false}', 'page': 0, 'sort_by': 'subscriber_count'}))
-# streamer_list = json.loads(g.text)
-# data2 = json.dumps(streamer_list, sort_keys=True, indent=4, separators=(',', ': '))
-# open("VTuber.json", "w").write(data2)
 
+def get_streamers():
+    for page in range(5):
+        g = requests.post(group_url, headers=headers, data=json.dumps(
+            {
+                'filter_state': '''{
+    "text": "",
+    "inc_old_group": false,
+    "retired": "all",
+    "following": false,
+    "notifications": false
+}''',
+                'page': page,
+                'sort_by': 'subscriber_count'
+            }))
+        streamer_list = json.loads(g.text)
+        for streamer in streamer_list["result"]:
+            yield (streamer["name"], streamer["streamer_id"])
+
+
+if __name__ == "__main__":
+    def pull_streamer_data_wrapper(pair):
+        pull_streamer_data(pair[0], pair[1], month_range)
+    with multiprocessing.Pool(4) as p:
+        p.map(pull_streamer_data_wrapper, get_streamers(), 1)
 
 # streamer_list= json.load(open("data/VTuber.json"))['result']
 # for streamer in streamer_list:
 #     streamer_name = streamer["name"]
 #     streamer_id = streamer["streamer_id"]
 #     Thread(target=pull_streamer_data, args=(streamer_name, streamer_id, month_range)).start()
-pull_video('y7_QWshqsw0')
+# pull_video('y7_QWshqsw0')
--- crawler.py
+++ crawler.py
@@ -9,28 +9,35 @@ group_url = "https://hiyoko.sonoj.net/f-slow/avtapi/search/streamer/fetchv3"
 list_url = "https://hiyoko.sonoj.net/f-slow/avtapi/strm/fetch_history_v2"
 data_url = "https://hiyoko.sonoj.net/f-slow/avtapi/video/fetch_summary"
 chatdata_url = "https://hiyoko.sonoj.net/f-slow/avtapi/video/fetch_summary_chatdata"
-headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.80 Safari/537.36', "Content-Type": "application/json"}
+headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.80 Safari/537.36',
+           "Content-Type": "application/json"}
 
 ############
-def pull_video_id(streamer_id, month_range):#从新到旧,爬取从from_month月到to_month月的视频ID
+
+
+def pull_video_id(streamer_id, month_range):  # 从新到旧,爬取从from_month月到to_month月的视频ID
     video_id_list = []
     month_range.sort()
-    for month in range(month_range[0],month_range[1] + 1):
-        r = requests.post(list_url, headers=headers, data=json.dumps({"streamer_id": streamer_id, "year": year, "month": month}))
+    for month in range(month_range[0], month_range[1] + 1):
+        r = requests.post(list_url, headers=headers, data=json.dumps(
+            {"streamer_id": streamer_id, "year": year, "month": month}))
 
-        #open("fetch_history.json","w").write(r.text)
+        # open("fetch_history.json","w").write(r.text)
         video_list = json.loads(r.text)
         for z in video_list["history_array"]:
-            video_id_list.append(z["video_id"])
-        
+            if z["event_type"] == "completed":
+                video_id_list.append(z["video_id"])
+
     return video_id_list
 
 ######
 
-def pull_video_data(video_id):#获得ID为video_id的视频的数据
-    l = requests.post(data_url, headers=headers, data=json.dumps({"video_id":video_id}))
+
+def pull_video_data(video_id):  # 获得ID为video_id的视频的数据
+    l = requests.post(data_url, headers=headers,
+                      data=json.dumps({"video_id": video_id}))
     v_data = json.loads(l.text)
-    
+
     v_data.pop("ch_id")
     v_data.pop("ch_type")
     v_data.pop("channel_misc")
@@ -46,8 +53,9 @@ def pull_video_data(video_id):#获得ID为video_id的视频的数据
     v_data.pop("streamer_thumbnail_url")
     v_data.pop("thumbnail_url")
     v_data.pop("view_count")
-    v_data["video_date"] = datetime.utcfromtimestamp(v_data["video_date"] / 1000 + 32400).strftime('%Y-%m-%d %H:%M:%S')#时间戳转换为日本时间
-    try:#获得直播结束时的赞数以及赞/人数比
+    v_data["video_date"] = datetime.utcfromtimestamp(
+        v_data["video_date"] / 1000 + 32400).strftime('%Y-%m-%d %H:%M:%S')  # 时间戳转换为日本时间
+    try:  # 获得直播结束时的赞数以及赞/人数比
         v_data["like_count_live_end"] = v_data["viewer_chart"]["like_count_list"][-1]
        # v_data.pop("viewer_chart")
     except TypeError:
@@ -55,14 +63,16 @@ def pull_video_data(video_id):#获得ID为video_id的视频的数据
     print("pulled video " + video_id)
     return v_data
 
-def pull_chat_data(video_id):#获得ID为video_id的聊天的数据
-    l = requests.post(chatdata_url, headers=headers, data=json.dumps({"video_id":video_id}))
+
+def pull_chat_data(video_id):  # 获得ID为video_id的聊天的数据
+    l = requests.post(chatdata_url, headers=headers,
+                      data=json.dumps({"video_id": video_id}))
     c_data = json.loads(l.text)
     print("pulled chat " + video_id)
     return c_data
 
 ##################
 
-#获取指定Vtuver信息
+# 获取指定Vtuver信息
 # g = requests.post(group_url, headers=headers, data=json.dumps({'filter_state': '{"selectedGroups": "cover", "text": "", "inc_old_group": false, "retired" : "all", "following":false, "notifications": false}', 'page': 0, 'sort_by': 'subscriber_count'}))
 # streamer_list = json.loads(g.text)
